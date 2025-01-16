60a41d2e4bab960a963e9005e048a7f24dbb3881
Harold Kemta <hxk8928@mavs.uta.edu>
2025-01-16 21:31:36 +0000

    printing

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
 
+void printarray ( int array[], int n ) {
+
+    for (int i = 1; i < n; i++) {
+
+        printf("%d", array[i]);
+        
+        }
+
+    printf("\n");
+
+}
+
 int main (void) {
 
-    printf( "original array: ")
+    int array[] = {12, 13, 11, 22, 31,3,3,};
+
+    int n = sizeof(array) / sizeof(array[0]);
+
+     printf( "original array: \n ");
+
+     printarray (array, n);
+
 }
