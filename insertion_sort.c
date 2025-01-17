7c119306e6f0d283f4c74d9536e3a8cd89566de2
Harold Kemta <hxk8928@mavs.uta.edu>
2025-01-17 22:41:05 +0000

    done

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,127 +1,94 @@
 #include <stdio.h>
 
-
-void printarray ( int array[], int n ) {
-
+void printarray(int array[], int n) {
     for (int i = 0; i < n; i++) {
-
         printf("%d ", array[i]);
-        
-        }
-
+    }
     printf("\n");
-
 }
 
-
-
-void insetionSort( int array[], int n ){
-
-    for (int i= 1; i > n; i++) {
-
-        // current element to be inserted
-
-        int key= array[i];
-
-         // if the number at position j is greater than the key ,move it up 
-
-         // think of j like the checker , the pointer 
-         
+void insertionSort(int array[], int n) {
+    for (int i = 1; i < n; i++) {  // corrected loop condition
+        int key = array[i];  // current element to be inserted
         int j = i - 1;
 
-        if (j >= 0 && array[j] > key ) {
-
-            // assaight the left to the right -- move the current number up , change the index by -1 and put the key where they last number was
-
-            array[j+1] = array[j]; 
-            j = j-1;            
+        // Move elements of array[0..i-1], that are greater than key, to one position ahead
+        while (j >= 0 && array[j] > key) {
+            array[j + 1] = array[j];
+            j = j - 1;
         }
-
-        array[j+1]= key; 
+        array[j + 1] = key;
     }
-
 }
-
 
 int linearSearch(int array[], int n, int target) {
-
-    printf( "linearSearch \n");
-
-    for ( int i = 0; i < n; i++) {
-
+    printf("Linear Search\n");
+    for (int i = 0; i < n; i++) {
         if (array[i] == target) {
-
-            return i;
-         }
-
+            return i;  // return the index where the element is found
+        }
     }
-
-      return -1;
-
+    return -1;  // return -1 if target is not found
 }
 
-    int binarySearch(int array[], int n, int target) {
+int binarySearch(int array[], int n, int target) {
+    printf("Binary Search\n");
 
-         printf( "Binary Search \n");
+    int low = 0, high = n - 1;
+    int middle;
 
-        int low , high , middle;
+    while (low <= high) {
+        middle = low + (high - low) / 2;  // calculate middle index
+        
+        if (array[middle] == target) {
+            return middle;  // target found
+        } else if (array[middle] < target) {
+            low = middle + 1;  // search the right half
+        } else {
+            high = middle - 1;  // search the left half
+        }
+    }
+    return -1;  // return -1 if target is not found
+}
 
-        low = 0 ;
+int recursiveBinarySearch(int array[], int low, int high, int target) {
+    if (low > high) {
+        return -1;  // base case: target not found
+    }
 
-        high = n-1;
+    int middle = low + (high - low) / 2;  // calculate middle index
 
-        middle = low + (high - low ) / 2;
-
-       while (low <= high) {
-    // Calculate middle index
-    int middle = low + (high - low) / 2;
-    
-    if (array[middle] == target) 
-        return middle; // Index found
-
-    if (array[middle] < target) {
-        low = middle + 1;  // Move to the right half
+    if (array[middle] == target) {
+        return middle;  // target found
+    } else if (array[middle] < target) {
+        return recursiveBinarySearch(array, middle + 1, high, target);  // search the right half
     } else {
-        high = middle - 1; // Move to the left half
+        return recursiveBinarySearch(array, low, middle - 1, target);  // search the left half
     }
 }
 
-
-    
-
-    }
-
-
-
-
-
-
-
-
-int main (void) {
-
-    int array[] = {12, 13, 11, 22, 31,3,3,};
-
+int main(void) {
+    int array[] = {12, 13, 11, 22, 31, 3, 3};
     int n = sizeof(array) / sizeof(array[0]);
 
-     printf( "original array: \n ");
+    printf("Original array: \n");
+    printarray(array, n);
 
-     printarray (array, n);
+    insertionSort(array, n);
 
-     insetionSort(array, n);
+    printf("Sorted array: \n");
+    printarray(array, n);
 
-     int target =3; 
+    int target = 22;
 
-     int foundIndex = linearSearch(array, n, target);
+    int foundIndex = linearSearch(array, n, target);
+    printf("Target found at index: %d (Linear Search)\n", foundIndex);
 
-         printf( "target found after %d Tries \n" , foundIndex);
+    int biIndex = binarySearch(array, n, target);
+    printf("Target found at index: %d (Binary Search)\n", biIndex);
 
-    int index =3; 
+    int rec_result = recursiveBinarySearch(array, 0, n - 1, target);
+    printf("Target found at index: %d (Recursive Binary Search)\n", rec_result);
 
-
-   int biIndex= binarySearch(array, n, target);
-
-        printf( "target found after %d Tries \n ", biIndex);
-
-
+    return 0;
 }
